ef61f7f37e3b1e1a569d8f6c471fb9951ca808ae
Kieran Emery <kizwiz@hotmail.co.uk>
2024-05-19 14:24:57 +0100

    Modifies the existing HTML tags in the Detials.cshtml page to add the CurrentFood properties.

--- VegWizGroceries/Pages/Details.cshtml.cs
+++ VegWizGroceries/Pages/Details.cshtml.cs
@@ -1,12 +1,18 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Xml.Linq;
+using VegWizGroceries.Models;
 
 namespace VegWizGroceries.Pages
 {
     public class DetailsModel : PageModel
     {
-        public void OnGet()
+        public List<GroceryItem> Foods = Inventory.ToList();
+        public GroceryItem CurrentFood;
+
+        public void OnGet(string name)
         {
+            CurrentFood = Foods.Find(food => food.Name.ToLower() == name.ToLower());
         }
     }
 }
