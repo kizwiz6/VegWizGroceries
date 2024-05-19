5e661b37194ac1bc8a21582286e5a28ac604209d
Kieran Emery <kizwiz@hotmail.co.uk>
2024-05-19 14:05:33 +0100

    Updates CSS for styling, Creates a Details razor page that links to a separate page for details about groceries.

---
+++ VegWizGroceries/Pages/Details.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace VegWizGroceries.Pages
+{
+    public class DetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
